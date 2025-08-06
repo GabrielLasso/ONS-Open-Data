6b28f9e37a5b14422df322f0cb89ce1512885f81
Gabriel Kuribara Lasso <glasso@libraenergia.com.br>
2025-08-06 16:56:04 -0300

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 ./gradlew wasmJsBrowserDistribution
 git checkout gh-pages
 cp -r composeApp/build/dist/wasmJs/productionExecutable/* .
-git add -a
+git add -A
 git commit -m "deploy"
 git push origin gh-pages
 git checkout master
